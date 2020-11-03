bfa1c815b3df6ab018ca9a90194c71ac674470cf
Alexander Matzen <alexander@amatzen.dk>
2020-11-03 10:47:29 +0100

    Added Hotkeys for GUI

---
+++ src/main/java/dk/sdu/mmmi/swe20/t1/g3/gui/Hotkeys.java
@@ -0,0 +1,11 @@
+package dk.sdu.mmmi.swe20.t1.g3.gui;
+
+public class Hotkeys {
+    // Movement Keys
+    final static public String MOVE_UP      = "W";
+    final static public String MOVE_LEFT    = "A";
+    final static public String MOVE_DOWN    = "S";
+    final static public String MOVE_RIGHT   = "D";
+
+
+}
