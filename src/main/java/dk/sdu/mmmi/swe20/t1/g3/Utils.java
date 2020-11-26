c835030cdd1da3e9698cfc3e0a778a56ab2aa1fe
Alexander Matzen <alexander@amatzen.dk>
2020-11-26 09:49:01 +0100

    Refactor

--- src/main/java/dk/sdu/mmmi/swe20/t1/g3/Utils.java
+++
@@ -1,9 +0,0 @@
-package dk.sdu.mmmi.swe20.t1.g3;
-
-public class Utils {
-    public static boolean isIntelliJ() {
-        // https://stackoverflow.com/a/37049420/6905947
-        String data = System.getProperty("java.class.path");
-        return data.contains("idea_rt.jar");
-    }
-}
