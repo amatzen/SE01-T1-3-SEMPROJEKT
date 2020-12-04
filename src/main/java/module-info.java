ff8de29d665b2f1c323cfb90f3f3eb57638796f9
Alexander Matzen <alexander@amatzen.dk>
2020-12-04 11:25:56 +0100

    Added module-info to control exports and requires

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module SE01T13SEMPROJEKT {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires io.reactivex.rxjava3;
+    requires com.google.gson;
+    requires java.desktop;
+
+    opens dk.sdu.mmmi.swe20.t1.g3.Views.Partials;
+
+    exports dk.sdu.mmmi.swe20.t1.g3.Views;
+    exports dk.sdu.mmmi.swe20.t1.g3.Views.Partials;
+    exports dk.sdu.mmmi.swe20.t1.g3;
+    exports worldofzuul;
+}
