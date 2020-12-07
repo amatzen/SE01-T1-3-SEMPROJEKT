366f9e5414f6a6dab9fa820b077ba04a9d24f744
Alexander Matzen <alexander@amatzen.dk>
2020-12-07 23:48:41 +0100

    Added Notification System

--- src/main/java/dk/sdu/mmmi/swe20/t1/g3/Utilities/FXUtils.java
+++ src/main/java/dk/sdu/mmmi/swe20/t1/g3/Utilities/FXUtils.java
@@ -1,6 +1,9 @@
 package dk.sdu.mmmi.swe20.t1.g3.Utilities;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.TextInputDialog;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 import java.util.Optional;
 
@@ -15,5 +18,14 @@ public class FXUtils {
         Optional<String> res = dialog.showAndWait();
 
         return res.orElse("");
+    }
+
+    public void emitNotify(String title, String text, int seconds) {
+        Notifications notificationBuilder = Notifications.create()
+                .title(title)
+                .text(text)
+                .hideAfter(Duration.seconds(seconds))
+                .position(Pos.BOTTOM_RIGHT);
+        notificationBuilder.showInformation();
     }
 }
