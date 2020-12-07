e15c49cb44c674b40a3c3362224350bc52973117
Alexander Matzen <alexander@amatzen.dk>
2020-12-07 10:25:32 +0100

    Moved onSceneChange from lambda to sep. method

--- src/main/java/dk/sdu/mmmi/swe20/t1/g3/Views/FXAppController.java
+++ src/main/java/dk/sdu/mmmi/swe20/t1/g3/Views/FXAppController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -17,6 +18,7 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class FXAppController implements Initializable {
@@ -38,7 +40,7 @@ public class FXAppController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        pubSub.addListener("fx_sceneChanged", (type, object) -> setSceneLabel(sceneController.getSceneBySlug((String) object).getName()));
+        pubSub.addListener("fx_sceneChanged", (type, object) -> onSceneChange(object));
         setSceneLabel(sceneController.getCurrentScene().getName());
 
         Player player = new Player(40, 50, 40, 70, Color.BLUE);
@@ -47,6 +49,7 @@ public class FXAppController implements Initializable {
             AppWindow.getScene().setOnKeyPressed(e -> {
                 switch (e.getCode()) {
                     case W, A, S, D -> player.handleKeyPress(e.getCode());
+                    case UP, LEFT, DOWN, RIGHT -> handleSceneKeyPress(e.getCode());
 
                     case E -> {
                         String whatToPickup = new FXUtils().prompt("Hvad skal jeg samle op?", "Skriv genstandens navn");
@@ -65,6 +68,24 @@ public class FXAppController implements Initializable {
             AppWindow.getChildren().add(player);
         });
 
+    }
+
+    private void onSceneChange() {
+        onSceneChange(null);
+    }
+    private void onSceneChange(Object payload) {
+        setSceneLabel(sceneController.getSceneBySlug((String) payload).getName());
+
+        // TODO: Lav ItemSpawner, evt. i Views/ItemFactory
+    }
+
+    private void handleSceneKeyPress(KeyCode keyCode) {
+        switch (keyCode) {
+            case UP -> pubSub.publish("executeCommand", "go op");
+            case LEFT -> pubSub.publish("executeCommand", "go venstre");
+            case DOWN -> pubSub.publish("executeCommand", "go ned");
+            case RIGHT -> pubSub.publish("executeCommand", "go højre");
+        }
     }
 
     @FXML
