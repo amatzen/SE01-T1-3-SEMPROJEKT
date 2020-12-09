05ca39a6f68dd818cbe921da88a17efa2bba782d
Alexander Matzen <alexander@amatzen.dk>
2020-12-09 20:08:20 +0100

    Added scene files

---
+++ src/main/resources/dk/sdu/mmmi/swe20/t1/g3/Views/Assets/fishTilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fishTilesheet" tilewidth="32" tileheight="32" tilecount="504" columns="36">
+ <image source="fishTilesheet.png" trans="ff00ff" width="1152" height="448"/>
+</tileset>
