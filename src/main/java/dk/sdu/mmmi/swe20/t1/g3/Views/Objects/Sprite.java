b3e66d54335be35d23b30dc3f076dc46b5bf16a1
Simon <simontbeldahl@outlook.dk>
2020-12-10 15:06:43 +0100

    Revert "Revert "This is a few changes""
    This reverts commit c6492d74c15b124bcfd0c771db75e4c74825694d.

--- src/main/java/dk/sdu/mmmi/swe20/t1/g3/Views/Objects/Sprite.java
+++ src/main/java/dk/sdu/mmmi/swe20/t1/g3/Views/Objects/Sprite.java
@@ -1,7 +1,10 @@
+
 package dk.sdu.mmmi.swe20.t1.g3.Views.Objects;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+
+import java.awt.image.BufferedImage;
 
 public class Sprite extends Rectangle {
     boolean dead = false;
@@ -19,4 +22,34 @@ public class Sprite extends Rectangle {
         setTranslateX(x);
         setTranslateY(y);
     }
+
+    private final BufferedImage SPRITESHEET = null;
+    private BufferedImage[][] spriteArray;
+    private final int TILE_SIZE = 32:
+    public int w:
+    public int h:
+    private int wSprite;
+    private int hSprite;
+
+    public Sprite(String file) {
+        w = TILE_SIZE;
+        h = TILE_SIZE;
+
+        System.out.println("loading: " + file "...");
+        SPRITESHEET = loadSprite(file);
+    }
+
+    public Sprite(String file, int w, int h) {
+        this.w = w;
+        this.h = h;
+
+        System.out.println("loading: " + file "...");
+        SPRITESHEET = loadSprite(file);
+
+
+
+    }
+
+
+
 }
